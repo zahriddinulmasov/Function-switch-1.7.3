2b27f168ffef460ecc4edb54367fc448d2327687
zahriddinulmasov <zahriddinus@gmail.com>
2022-06-05 19:44:31 +0500

    Corrected

--- js/index.js
+++ js/index.js
@@ -2,18 +2,28 @@ const elForm = document.querySelector(".form");
 const elFormInput = document.querySelector(".form__input");
 const elResult = document.querySelector(".result");
 
-elForm.addEventListener("submit", function(evt) {
-    evt.preventDefault();
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
 
-    const inputValue = elFormInput.value;
+  const inputValue = elFormInput.value;
 
-    if(inputValue % 3 === 0) {
-        elResult.textContent = "Fizz";
-    } else if(inputValue % 5 === 0) {
-        elResult.textContent = "Buzz";
-    } else if(inputValue % 3 === 0 && inputValue % 5 === 0) {
-        elResult.textContent = "FIzzBuzz";
-    } else if(inputValue % 3 !== 0 && inputValue % 5 !== 0) {
-        elResult.textContent = `${inputValue}`;
-    }
-})
+  if (inputValue % 3 === 0 && inputValue % 5 === 0) {
+    elResult.textContent = "FIzzBuzz";
+  } else if (inputValue % 5 === 0) {
+    elResult.textContent = "Buzz";
+  } else if (inputValue % 3 === 0) {
+    elResult.textContent = "Fizz";
+  } else if (inputValue % 3 !== 0 && inputValue % 5 !== 0) {
+    elResult.textContent = `${inputValue}`;
+  }
+});
+
+if (inputValue % 3 === 0) {
+  elResult.textContent = "Fizz";
+} else if (inputValue % 5 === 0) {
+  elResult.textContent = "Buzz";
+} else if (inputValue % 3 === 0 && inputValue % 5 === 0) {
+  elResult.textContent = "FIzzBuzz";
+} else if (inputValue % 3 !== 0 && inputValue % 5 !== 0) {
+  elResult.textContent = `${inputValue}`;
+}
